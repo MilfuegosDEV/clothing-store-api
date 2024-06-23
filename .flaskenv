a863bc8b94195a64e0f63662aabd45ab217a3be7
MilfuegosDEV <milfuegosdev@gmail.com>
2024-06-23 14:18:47 -0600

    chore: Update JWT configuration and database queries

--- .flaskenv
+++ .flaskenv
@@ -6,5 +6,8 @@ DB_USER=postgres
 DB_PASSWORD=admin
 DB_NAME=clothing_store
 
-JWT_SECRET_KEY=secret
+JWT_SECRET_KEY=secret 
+JWT_ACCESS_TOKEN_EXPIRES=1 # 1h 
+JWT_REFRESH_TOKEN_EXPIRES=30 # 30d
 
+# Path: .env
--- src/infrastructure/config/__init__.py
+++ src/infrastructure/config/__init__.py
@@ -1,5 +1,5 @@
 import os
-
+from datetime import timedelta
 
 class Config:
     """
@@ -19,3 +19,9 @@ class Config:
     SQLALCHEMY_ECHO = False
 
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(
+        hours=float(os.getenv("JWT_ACCESS_TOKEN_EXPIRES"))
+    )
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(
+        days=float(os.getenv("JWT_REFRESH_TOKEN_EXPIRES"))
+    )
